6cc42b1dd3c5c172675e19e5aa3ef60af6aae466
doamatto <hello@doamatto.xyz>
2020-06-19 23:14:56 -0700

    Added locales support

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const tmi = require("tmi.js");
-const conf = require("./config.json");
+const conf = require("./config.json"); // Config for the bot
+const fetch = require("node-fetch"); // Used for HTTP requests
+const db = require("sqlite"); // Used for holding the members the bot needs to listen to
 const client = new tmi.client(conf.opts);
+
+var locale;
 
 client.on("message", (target, context, msg, self) => {
   if (self) return; // Prevents bot talking to itself
@@ -13,8 +17,27 @@ client.on("message", (target, context, msg, self) => {
   }
 });
 
+function locale_set() {
+  fetch(`https://api.twitch.tv/helix/channels?broadcaster_id=${broadcaster_id}`, {
+    method: 'get',
+    headers: {
+      "client-id": `${conf.client-id}`,
+      "Authorization": `Bearer ${conf.client-id}`
+    },
+    body: JSON.stringify(body)
+  }).then(res => res.json()).then(json => {
+    for(var i=0; i<msgs.length; i++) {
+      if(json.data.broadcaster_language == msgs[i]) {
+        return locale = msgs[i];
+      } else {
+        return locale = nil;
+      }
+    }
+  });
+} // Not tested; fetches broadcast langauage, ensures there's a locale, and sets it for the stream
+
 client.on("connected", (addr, port) => {
-  console.log(`* Connected to ${addr}:${port}`)
+  console.log(`* Connected to ${addr}:${port}`);
 });
 
 client.connect(); // Connect the bot
